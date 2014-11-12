a095d33a6b583c86b05a78e6587a0b731b171af7
Nate Woolls <nwoolls@gmail.com>
2014-11-12 00:44:01 -0500

    Initial commit of Network Device scanning
    Determines local interface details and IP ranges

--- src/miner-command.c
+++ src/miner-command.c
@@ -7,9 +7,11 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "weechat-plugin.h"
 #include "miner-plugin.h"
 #include "miner-device.h"
+#include "miner-network.h"
 
 static
 int miner_command_device(void *data, struct t_gui_buffer *buffer, int argc,
@@ -40,6 +42,29 @@ int miner_command_device(void *data, struct t_gui_buffer *buffer, int argc,
     return WEECHAT_RC_OK;
 }
 
+static
+int miner_command_network(void *data, struct t_gui_buffer *buffer, int argc,
+        char **argv, char **argv_eol)
+{
+    if (argc < 2)
+        return WEECHAT_RC_ERROR;
+
+    const bool list_details = weechat_strcasecmp(argv[1], "listfull") == 0;
+
+    if ((weechat_strcasecmp(argv[1], "list") == 0) || list_details)
+    {
+        miner_network_list(list_details);
+    }
+    else if (weechat_strcasecmp(argv[1], "scan") == 0)
+    {
+        miner_network_scan();
+    }
+    else
+        return WEECHAT_RC_ERROR;
+
+    return WEECHAT_RC_OK;
+}
+
 void miner_command_init()
 {
     weechat_hook_command (
@@ -53,4 +78,16 @@ void miner_command_init()
 							 "\n"
 							 "Examples:\n"),
 						  NULL, &miner_command_device, NULL);
+
+
+    weechat_hook_command (
+            "network",
+            N_("list or scan network devices"),
+            N_("list|listfull|scan"),
+            N_("    list: list known network devices\n"
+               "listfull: list known network devices (with details)\n"
+               "    scan: scan network for mining devices\n"
+               "\n"
+               "Examples:\n"),
+            NULL, &miner_command_network, NULL);
 }
---
+++ src/miner-network.h
@@ -0,0 +1,10 @@
+#ifndef MultiMiner_miner_network_h
+#define MultiMiner_miner_network_h
+
+extern
+void miner_network_scan();
+
+extern
+void miner_network_list(const bool details);
+
+#endif
